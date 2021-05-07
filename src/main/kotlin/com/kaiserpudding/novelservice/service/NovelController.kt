5f1af1a1c97f6e8133d0060497b2c9020d1abb6d
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2021-05-07 16:52:05 +0200

    Add "novel" path to NovelController

--- src/main/kotlin/com/kaiserpudding/novelservice/service/NovelController.kt
+++ src/main/kotlin/com/kaiserpudding/novelservice/service/NovelController.kt
@@ -5,9 +5,11 @@ import com.kaiserpudding.novelservice.api.TriggerResult
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/novel")
 class NovelController {
 
     companion object {
