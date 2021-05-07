7547f6207c5a54ae59669f194a6d8f969dc6e380
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2021-05-07 16:54:41 +0200

    Add GET /novel/{id} endpoint to fetch novels

---
+++ src/main/kotlin/com/kaiserpudding/novelservice/api/Novel.kt
@@ -0,0 +1,6 @@
+package com.kaiserpudding.novelservice.api
+
+data class Novel(
+    val status: String,
+    val downloadUrl: String
+)
--- src/main/kotlin/com/kaiserpudding/novelservice/service/NovelController.kt
+++ src/main/kotlin/com/kaiserpudding/novelservice/service/NovelController.kt
@@ -1,8 +1,11 @@
 package com.kaiserpudding.novelservice.service
 
+import com.kaiserpudding.novelservice.api.Novel
 import com.kaiserpudding.novelservice.api.TriggerConfig
 import com.kaiserpudding.novelservice.api.TriggerResult
 import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,7 +21,13 @@ class NovelController {
 
     @PostMapping("/trigger")
     fun trigger(@RequestBody config: TriggerConfig): TriggerResult {
-        LOG.info("POST trigger '$config'" )
+        LOG.info("POST novel/trigger. config: '$config'" )
         return TriggerResult(1)
+    }
+
+    @GetMapping("/{id}")
+    fun getNovel(@PathVariable(name = "id") id: Long): Novel {
+        LOG.info("GET novel/$id")
+        return Novel("done", "url")
     }
 }
