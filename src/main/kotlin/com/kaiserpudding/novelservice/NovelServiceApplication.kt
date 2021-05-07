ea17d3b697cd259a46ae0f8fd08b6174508344a5
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2021-05-07 15:39:27 +0200

    Generate project with spring initializer

---
+++ src/main/kotlin/com/kaiserpudding/novelservice/NovelServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.kaiserpudding.novelservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NovelServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<NovelServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/kaiserpudding/novelservice/NovelServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kaiserpudding.novelservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NovelServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
